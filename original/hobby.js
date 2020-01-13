4020e19238c154b8b64c577f806e8dcba770151c
yamaguchi-kotaro6 <m011831355@edu.teu.ac.jp>
2020-01-14 00:02:09 +0900

    自主制作　途中経過12

---
+++ original/hobby.js
@@ -0,0 +1,30 @@
+//サイトからほぼ引用(一部改変)
+
+function topButton(elmId, duration) {
+
+    //トップに戻るボタンの要素の取得
+    var topButton = document.getElementById(elmId);
+
+    topButton.addEventListener("click", function(e){
+
+        //デフォルトの動作の制御
+        e.preventDefault();
+
+        var begin = new Date() - 0;
+        var yOffset = window.pageYOffset;
+        var timer= setInterval(function() {
+            var current = new Date() - begin;
+            if (current > duration) {
+                clearInterval(timer);
+                current = duration;
+            }
+
+            //スクロール位置を単位時間で変更する
+            window.scrollTo(0, yOffset * (1 - current / duration))
+        }, 10);
+    }, false)
+
+}
+
+// 使用例 トップに戻るボタンの id とアニメーションにかかる時間をミリ秒で指定
+topButton("return-top", 400)
